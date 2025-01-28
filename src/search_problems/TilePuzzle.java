60587fbd3fde1d1ef9ec946ebea2e2bc578f0a05
Kayan Laswell <kcclaswell@gmail.com>
2025-01-28 11:20:26 -0500

    Change LinkedLists to ArrayLists

--- src/search_problems/TilePuzzle.java
+++ src/search_problems/TilePuzzle.java
@@ -5,26 +5,28 @@ import core_search.Tuple;
 
 import java.util.*;
 
-public class TilePuzzle implements Problem<LinkedList<Integer>,int> {
-    private final LinkedList<Integer> INITIAL_STATE = new LinkedList<Integer>(Arrays.asList(7, 2, 4, 5, 0, 6, 8, 3, 1));
+public class TilePuzzle implements Problem<ArrayList<Integer>,int> {
+    private final ArrayList<Integer> INITIAL_STATE = new ArrayList<>(Arrays.asList(7, 2, 4, 5, 0, 6, 8, 3, 1));
 
-    private final LinkedList<Integer> GOAL_STATE = new LinkedList<Integer>(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8));
+    private final ArrayList<Integer> GOAL_STATE = new ArrayList<>(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8));
 
-    public LinkedList<Integer> initialState(){
+    private void buildTransitionModel(){
+
+    }
+
+    public ArrayList<Integer> initialState(){
         return INITIAL_STATE;
     }
 
-    public LinkedList<Integer> goalState(){
+    public ArrayList<Integer> goalState(){
         return GOAL_STATE;
     }
 
-    public actions(){
 
-    }
 
     private final Map<String, List<Tuple<String, String>>> transitionModel = new HashMap<>();
     private void buildTransitionModel(){
-
+        while(INITIAL_STATE)
     }
 
 }
