15c84241fbd59009672b1db520084a8b1fcbbeeb
Kayan Laswell <kcclaswell@gmail.com>
2025-02-02 14:03:58 -0500

    Complete STP_UCS with path cost estimate

--- src/search_solutions/STP_UCS.java
+++ src/search_solutions/STP_UCS.java
@@ -4,28 +4,22 @@ import core_search.BaseSearch;
 import core_search.Node;
 import core_search.SortedQueue;
 import search_problems.TilePuzzle;
-import search_problems.Travel;
+
 
 import java.util.Comparator;
 import java.util.List;
 
 public class STP_UCS extends BaseSearch<List<Integer>,String> {
     public STP_UCS (){
-        super(new TilePuzzle(), new SortedQueue<>(new STP_UCS.ComparePathCost()));
+        super(new TilePuzzle(), new SortedQueue<>(new ComparePathCost()));
     }
     public static void main(String[] args){
         STP_UCS t = new STP_UCS();
         t.search();
     }
-    public static class ComparePathCost implements Comparator<Node<String,String>> {
-        public int compare (Node<String, String> o1, Node<String, String> o2){
-            if(o1.getPathCost() < o2.getPathCost()){
-                return -1;
-            }else if (o1.getPathCost()==o2.getPathCost()){
-                return 0;
-            }else{
-                return 1;
-            }
+    public static class ComparePathCost implements Comparator<Node<List<Integer>, String>> {
+        public int compare(Node<List<Integer>, String> o1, Node<List<Integer>, String> o2) {
+            return Double.compare(o1.getPathCost(), o2.getPathCost());
         }
     }
 }
