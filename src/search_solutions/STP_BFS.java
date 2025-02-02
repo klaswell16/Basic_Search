39c28d60af40b6702a78492519cf76cc5f3650ce
arscott11 <152930029+arscott11@users.noreply.github.com>
2025-02-01 21:50:24 -0500

    Update STP_BFS.java

--- src/search_solutions/STP_BFS.java
+++ src/search_solutions/STP_BFS.java
@@ -3,13 +3,15 @@ package search_solutions;
 import core_search.BaseSearch;
 import core_search.FIFOQueue;
 import search_problems.TilePuzzle;
+import java.util.List;
 
-public class STP_BFS extends BaseSearch<String, Integer> {
-    public STP_BFS(){
+public class STP_BFS extends BaseSearch<List<Integer>, String> {
+    
+    public STP_BFS() {
         super(new TilePuzzle(), new FIFOQueue<>());
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         STP_BFS t = new STP_BFS();
         t.search();
     }
