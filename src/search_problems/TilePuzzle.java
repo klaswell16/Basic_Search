d97b864184b9d9c61a7b4a7fe83b2902837cd679
arscott11 <152930029+arscott11@users.noreply.github.com>
2025-02-01 21:49:26 -0500

    Got working

--- src/search_problems/TilePuzzle.java
+++ src/search_problems/TilePuzzle.java
@@ -6,54 +6,49 @@ import core_search.Tuple;
 import java.util.*;
 
 public class TilePuzzle implements Problem<List<Integer>,String> {
-    private final ArrayList<Integer> INITIAL_STATE = new ArrayList<>(
-            List.of(7, 2, 4, 5, 0, 6, 8, 3, 1)
-    );
-
-    private final ArrayList<Integer> GOAL_STATE = new ArrayList<>(
-            List.of(0, 1, 2, 3, 4, 5, 6, 7, 8)
-    );
+    private final int SIZE = 3;
+    private final List<Integer> INITIAL_STATE = new ArrayList<>(Arrays.asList(7, 2, 4, 5, 0, 6, 8, 3, 1));
+    private final List<Integer> GOAL_STATE = new ArrayList<>(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8));
 
 
-    public List<Tuple<List<Integer>, String>> execution(List<Integer>currentState){
+
+    public List<Tuple<List<Integer>, String>> execution(List<Integer> currentState) {
         List<Tuple<List<Integer>, String>> moves = new ArrayList<>();
-
-        int indexZero = INITIAL_STATE.indexOf(0);
-        int[][] directions = {{-1, 0},{1, 0},{0, -1},{0, 1}};
-        String[] directionNames = {"Up","Down","Left","Right"};
-        int SIZE = 3;
+        int indexZero = currentState.indexOf(0);
         int row = indexZero / SIZE;
         int column = indexZero % SIZE;
 
-        for (int i = 0; i < directions.length; i++){
-            int newRow = row + directions[i][0];
-            int newColumn = column + directions[i][1];
+        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        String[] directionNames = {"Up", "Down", "Left", "Right"};
 
-            if (newRow >= 0 && newRow > SIZE && newColumn >= 0 && newColumn > SIZE){
+        for (int i = 0; i < directions.length; i++) {
+            int newRow = row + directions[i][0];
+            int newCol = column + directions[i][1];
+
+            if (newRow >= 0 && newRow < SIZE && newCol >= 0 && newCol < SIZE) {
                 List<Integer> newState = new ArrayList<>(currentState);
-                Collections.swap(newState, indexZero, newRow * SIZE + newColumn);
+                Collections.swap(newState, indexZero, newRow * SIZE + newCol);
                 moves.add(new Tuple<>(newState, directionNames[i], 1));
             }
         }
         return moves;
     }
 
-    @Override
-    public void printState(List<Integer> state) {
-        System.out.println(state);
-    }
-
-
-    public ArrayList<Integer> initialState(){
+    public List<Integer> initialState() {
         return INITIAL_STATE;
     }
 
-    public ArrayList<Integer> goalState(){
+    public List<Integer> goalState() {
         return GOAL_STATE;
     }
 
-
-
-
-
+    public void printState(List<Integer> state) {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(state.get(i * SIZE + j) + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
 }
